5c1618f5d915b2435bc7ffc249d60b4b9768f8a6
Kenia Segura Abá <keniaesegura@gmail.com>
2024-06-05 12:20:28 -0400

    Modified to correct batches in other traits too

--- code/0b_raw_data_correction_linear_b.R
+++ code/0b_raw_data_correction_linear_b.R
@@ -13,49 +13,84 @@ system('module purge; module load GCC/11.2.0  OpenMPI/4.1.1  R/4.3.1')
 
 # R v 4.3.1
 library(emmeans)
+library(dplyr)
 
 # Load the data
 df <- read.csv('../data/double_mutant_fitness_data_05312024.txt', sep='\t', header=T)
+labels <- c('GN', 'PG', 'DTB', 'LN', 'DTF', 'SN', 'WO', 'FN', 'SPF', 'TSC', 'SH')
 
-# Collect fitted values for TSC
-res <- data.frame()
+counter <- 1
+for (label in labels){
+    print(paste0('LABEL ', label, ' -----------------------------------------'))
 
-# Apply spatial analysis per set
-for (set in unique(df$Set)) {
-    set_df <- df[df$Set == set,] # set data
+    # Collect fitted values for each label
+    if (counter == 1) res <- data.frame()
+    if (counter != 1) res2 <- data.frame()
 
-    # Remove rows with missing values in TSC (total seed count)
-    set_df <- set_df[!is.na(set_df$TSC),]
+    # Run linear regression per set
+    for (set in unique(df$Set)) {
+        print(paste0('Set ', set))
+        set_df <- df[df$Set == set,] # set data
 
-    # Set WT as the reference level
-    set_df$Genotype <- factor(set_df$Genotype, levels=c('WT', 'MA', 'MB', 'DM'))
+        # Remove rows with missing values in label
+        set_df <- set_df[!is.na(set_df[,label]),]
 
-    # For sets grown on multiple flats, include flat as random effect
-    if (length(unique(set_df$Flat)) > 1) {
-        # Fit a linear model
-        model <- lme4::lmer(TSC ~ Genotype + (1|Column) + (1|Row) + (1|Flat), data = set_df)
+        # For sets grown on multiple flats, include flat as random effect
+        if (length(unique(set_df$Flat)) > 1) {
+            # Set WT as the reference level
+            set_df$Genotype <- factor(set_df$Genotype, levels=c('WT', 'MA', 'MB', 'DM'))
 
-        # Save the model
-        saveRDS(model, paste0('../output/0_raw_data_correction/Set_', set, '_brianna_model.rds'))
+            # Fit a linear model
+            model <- lme4::lmer(paste0(label, ' ~ Genotype + (1|Column) + (1|Row) + (1|Flat)'), data = set_df)
 
-        # Calculate estimated means
-        emm <- as.data.frame(emmeans(model, ~ Genotype))
-        out <- dplyr::left_join(set_df, emm, by='Genotype')
-        res <- rbind(res, out)
+            # Save the model
+            saveRDS(model, paste0('../output/0_raw_data_correction/Set_', set,
+                '_', label, '_brianna_model.rds'))
+
+            # Calculate estimated means
+            emm <- as.data.frame(emmeans(model, ~ Genotype))
+            emm <- rename_with(emm, ~ paste0(label, "_", .))
+            out <- left_join(set_df, emm, by=c('Genotype'=paste0(label, '_Genotype')))
+            if (counter == 1) res <- rbind(res, out)
+            if (counter != 1) res2 <- rbind(res2, out)
         }
-    if (length(unique(set_df$Flat)) == 1) {
-        # Fit a linear model
-        model <- lme4::lmer(TSC ~ Genotype + (1|Column) + (1|Row), data = set_df)
+        if (length(unique(set_df$Flat)) == 1) {
+            # Set WT as the reference level
+            set_df$Genotype <- factor(set_df$Genotype, levels=c('WT', 'MA', 'MB', 'DM'))
+            tryCatch({
+                # Fit a linear model
+                model <- lme4::lmer(paste0(label, ' ~ Genotype + (1|Column) + (1|Row)'), data = set_df)
 
-        # Save the model
-        saveRDS(model, paste0('../output/0_raw_data_correction/Set_', set, '_brianna_model.rds'))
+                # Save the model
+                saveRDS(model, paste0('../output/0_raw_data_correction/Set_',
+                    set, '_', label, '_brianna_model.rds'))
 
-        # Calculate estimated means
-        emm <- as.data.frame(emmeans(model, ~ Genotype))
-        out <- dplyr::left_join(set_df, emm, by='Genotype')
-        res <- rbind(res, out)
+                # Calculate estimated means
+                emm <- as.data.frame(emmeans(model, ~ Genotype))
+                emm <- rename_with(emm, ~ paste0(label, "_", .))
+                out <- left_join(set_df, emm, by=c('Genotype'=paste0(label, '_Genotype')))
+                if (counter == 1) res <- rbind(res, out)
+                if (counter != 1) res2 <- rbind(res2, out)
+            }, error = function(e) {
+                print(e)
+            }, warning = function(w) {
+                print(w)
+            })
+        }
     }
+
+    if (counter != 1) {
+        print(dim(res))
+        print(dim(res2))
+        res <- left_join(res, res2, by=c('Set', 'Flat', 'Column', 'Row', 
+            'Number', 'Type', 'Genotype', 'Subline', 'MA', 'MB'), keep=F)
+    }
+    counter <- counter + 1
 }
 
 # Save the corrected data
-write.csv(res, '../data/double_mutant_fitness_data_05312024_TSC_corrected_brianna.txt', row.names=F, quote=F)
+res <- res[, !grepl("\\.x$|\\.y$", names(res))]
+res <- left_join(df, res, by=c('Set', 'Flat', 'Column', 'Row', 'Number', 'Type',
+    'Genotype', 'Subline', 'MA', 'MB'))
+res <- rename_with(res, ~ sub("\\.x$", "", .), .cols = ends_with(".x"))
+write.table(res, paste0('../data/double_mutant_fitness_data_05312024_all_corrected_brianna.txt'), row.names=F, quote=F, sep='\t')
